50996087a7623b01124801a7079fcee375172cea
Korovsky <sestra.che@gmail.com>
2018-07-14 16:06:09 +0300

    Changed name of directory for portfolio samples

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
 
 class Work(models.Model):
     title = models.CharField(max_length=200, default='Untitled')
-    sample = models.ImageField(upload_to='sample-images')
+    sample = models.ImageField(upload_to='samples')
     description = tinymce_models.HTMLField()
 
     def add_work(self):
